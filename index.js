917b034775b09964c90f5c25630f810d1c8ab3ad
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-20 23:49:14 +0530

    Add eventListener

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function saveLead(){
+// function saveLead(){
+//     console.log("save")
+// }
+
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function(){
     console.log("save")
-}
+})
