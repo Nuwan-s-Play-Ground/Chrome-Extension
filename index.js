a94f4ef9907248bc127cfa9932b5378b3d43e9ba
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-21 00:41:36 +0530

    Clear the text field

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let myLeads = [12,45,"eterg"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    //console.log(myLeads)
+    inputEl.value = ""
     renderLeads();
 })
 
