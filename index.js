a8baace947a61a6510e3eaf8f5f87e84727b89a2
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-21 23:24:36 +0530

    Save the lead from localstorage

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+localStorage.setItem("myname", "Nuwan")
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads();
 })
 
