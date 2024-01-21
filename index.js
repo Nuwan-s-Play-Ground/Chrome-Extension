3ff560edac7a6068c566d9d48afbeb47a2ff1216
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-21 23:45:58 +0530

    Make the delete buttom work

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
-
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads();
-})
 
 function renderLeads() {
     let listItems = ""
@@ -27,7 +21,21 @@ function renderLeads() {
                 </a>
             </li>
         `
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
-}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads();
+})
+
 
